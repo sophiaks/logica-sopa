2826b7b2a5954cfe931e52c230b3de9e0b9ea75b
Sophia Kerber Shigueoka <sophia.shigueoka@gmail.com>
2022-08-29 13:44:34 -0300

    removing whitespaces

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ sig_list = []
 
 formula_raw = sys.argv[1]
 formula = formula_raw.strip()
+form = re.sub(r'(\s|\u180B|\u200B|\u200C|\u200D|\u2060|\uFEFF)+', '', formula)
 
-numbers = re.findall(r'[0-9]+', formula)
-sigs = re.findall(r'[+-]+', formula)
-num_sig  = re.split(r'(\D)', formula)
+
+numbers = re.findall(r'[0-9]+', form)
+sigs = re.findall(r'[+-]+', form)
+num_sig  = re.split(r'(\D)', form)
 if '' in num_sig:
     num_sig.remove('')
 if ' ' in num_sig:
     num_sig.remove(' ')
+
 
 def check_first(list):
     if list[0] in sig_dict:
@@ -34,10 +37,30 @@ def check_syntax(num_list, sig_list):
         raise Exception("A formula nao contem numeros")
 
 sig_list, num_list = check_first(num_sig)
+
+if '' in num_list:
+    num_list.remove('')
+if ' ' in num_list:
+    num_list.remove(' ')
+
+if '' in sig_list:
+    sig_list.remove('')
+if ' ' in sig_list:
+    sig_list.remove(' ')
+
+
 check_syntax(num_list, sig_list)
 res = 0
 minus = False
+
+#print(sig, num)
+
 for num, sig in itertools.zip_longest(num_list, sig_list):
+    #print(num_list)
+    #print(sig_list)
+    if num == ' ' or num == '':
+        pass
+    #print(f"num: {num}")
     num = int(num)
     if minus == True:
         res -= num
