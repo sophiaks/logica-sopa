b4209ae7b650e9dcb28907a0ee9e7521382bf91d
Sophia Kerber Shigueoka <sophia.shigueoka@gmail.com>
2022-08-29 13:05:07 -0300

    restarting tokenizer

--- main.py
+++ main.py
@@ -1,53 +1,80 @@
 import re
 import sys
 import itertools
+from urllib.parse import parse_qs
 
-sig_dict = ['+', '-']
-num_list = []
-sig_list = []
+source = sys.argv[1]
 
-formula_raw = sys.argv[1]
-formula = formula_raw.strip()
+def isNumber(num):
+    print("Checking if token is number")
+    try:
+        value = float(num)
+        return True
+    except ValueError:
+        return False
 
-numbers = re.findall(r'[0-9]+', formula)
-sigs = re.findall(r'[+-]+', formula)
-num_sig  = re.split(r'(\D)', formula)
-if '' in num_sig:
-    num_sig.remove('')
-if ' ' in num_sig:
-    num_sig.remove(' ')
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
 
-def check_first(list):
-    if list[0] in sig_dict:
-        raise Exception("Primeiro valor tem sinal")
-    else:
-        num_list = list[::2]
-        sig_list = list[1::2]
-        return sig_list, num_list
+class Tokenizer:
+    def __init__(self, source):
+        self.source = source
+        self.position = 0
+        self.next = None
 
-def check_syntax(num_list, sig_list):
-    if len(num_list) != len(sig_list) + 1:
-        raise Exception("Existem elementos faltando")
-    if len(sig_list) == 0:
-        raise Exception("A formula nao contem sinais")
-    if len(num_list) == 0:
-        raise Exception("A formula nao contem numeros")
+    def selectNext(self):
+        token = self.source[self.position]
+        temp_next = ""
+        if isNumber(token):
+            if self.source[self.position + 1] == '+' or self.source[self.position + 1] == '-':
+                self.next = Token('INT', int(temp_next))
+                print(f"Updating next token to {self.next}")
+            else:
+                temp_next += token
+                print(f"Updating temp token to {temp_next}")
+        elif token == '+':
+            self.next = Token('PLUS', '+')
+            print(f"Updating next token to {self.next}")
+        elif token == '-':
+            self.next = Token('MINUS', '-')
+            print(f"Updating next token to {self.next}")
+        elif self.position > len(self.source):
+            self.next = Token('EOF', '')
+            print(f"Reached end of file")
+        self.position += 1
+            
 
-sig_list, num_list = check_first(num_sig)
-check_syntax(num_list, sig_list)
-res = 0
-minus = False
-for num, sig in itertools.zip_longest(num_list, sig_list):
-    num = int(num)
-    if minus == True:
-        res -= num
-    elif minus == False: 
-        res += num
-    if sig == '-':
-        minus = True
-    if sig == '+':
-        minus = False
-    if sig is None:
-        minus = False
+class Parser:
+    @staticmethod
+    def parseExpression(tokenizer):
+        print("Beginning to parse expression...")
+        token_type = tokenizer.next.type
+        res = 0
+        if token_type == 'INT':
+            res = int(tokenizer.next)
+            tokenizer.selectNext()
+            while token_type in ['MINUS', 'PLUS']:
+                if token_type == 'PLUS':
+                    tokenizer.selectNext()
+                    if tokenizer.next.type == 'INT':
+                        res += tokenizer.next.value
+                    else: 
+                        raise Exception("Syntax Error")
+                if token_type == 'MINUS':
+                    tokenizer.selectNext()
+                    if tokenizer.next.type == 'INT':
+                        res -= tokenizer.next.value
+                    else:
+                        raise Exception("Syntax Error")
+                tokenizer.selectNext()
+            return res
+    
+    def run(code):
+        print("Runnning Code")
+        Parser.tokenizer = Tokenizer(code)
+        Parser.parseExpression(Parser.tokenizer)
 
-print(res)
+
+Parser.run(source)
