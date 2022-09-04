11b40fd3af6f67eefdf867880acf457eb622acee
Sophia Kerber Shigueoka <sophia.shigueoka@gmail.com>
2022-09-04 18:06:04 -0300

    Test fails but issue does not appear

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ class Tokenizer:
 
         if self.source[self.position] == '*':
             self.next = Token('MULT', '*')
-            self.position += 1
+            self.position += 1 
         
         if self.source[self.position] == '/':
             self.next = Token('DIV', '/')
